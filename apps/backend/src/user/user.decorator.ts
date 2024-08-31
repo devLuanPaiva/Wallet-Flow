585d5adc3be110374a1107a1af765eb8dc21ed2c
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-08-31 07:56:16 -0300

    fix: move files for user folder

--- apps/backend/src/user/user.decorator.ts
+++ apps/backend/src/user/user.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const Logged = createParamDecorator(
-  (_data: unknown, ctx: ExecutionContext) => {
+export const UserLogged = createParamDecorator(
+  (_date: unknown, ctx: ExecutionContext) => {
     const req = ctx.switchToHttp().getRequest();
     return req.user;
   },
--- apps/backend/src/user/user.middleware.ts
+++ apps/backend/src/user/user.middleware.ts
@@ -1,22 +1,24 @@
 import { HttpException, Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, NextFunction, Response } from 'express';
-import { UserRepository } from 'src/user/user.repository';
+import { NextFunction, Request, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
+import { UserRepository } from './user.repository';
 import { UserI } from '@wallet/core';
 
 @Injectable()
-export class AuthMiddleware implements NestMiddleware {
+export class UserMidddleware implements NestMiddleware {
   constructor(private readonly repo: UserRepository) {}
+
   async use(req: Request, res: Response, next: NextFunction) {
     const token = req.headers['authorization']?.replace('Bearer ', '');
     if (!token) {
-      throw new HttpException('Token não fornecido!', 401);
+      throw new HttpException('Token não informado', 401);
     }
+
     const payload = jwt.verify(token, process.env.JWT_SECRET) as UserI;
     const user = await this.repo.searchEmail(payload.email);
 
     if (!user) {
-      throw new HttpException('Usuário não encontrado!', 401);
+      throw new HttpException('Usuário não encontrado', 401);
     }
     (req as any).user = user;
     next();
