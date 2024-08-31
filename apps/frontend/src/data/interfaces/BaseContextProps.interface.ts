d0beaab4a47717c8843a5691cb66d52e81678260
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-08-31 09:32:47 -0300

    feat: add User context

--- apps/frontend/src/data/interfaces/BaseContextProps.interface.ts
+++ apps/frontend/src/data/interfaces/BaseContextProps.interface.ts
@@ -8,3 +8,8 @@ export interface SectionContextProps extends BaseContextProps {
   createSection: (jwt: string) => void;
   clearSection: () => void;
 }
+export interface UserContextProps extends BaseContextProps {
+  login: (user: Partial<UserI>) => Promise<void>
+  register: (user: UserI) => Promise<void>
+  logout: () => void;
+}
