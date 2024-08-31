2aa6b8ccf263714339b87a5a240940c5d548bd27
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-08-31 11:07:05 -0300

    feat: add auth page

---
+++ apps/frontend/src/app/(pages)/layout.tsx
@@ -0,0 +1,12 @@
+import { SectionProvider } from "@/data/contexts/SectionContext";
+import { UserProvider } from "@/data/contexts/UserContext";
+
+export default function Layout({ children }: any) {
+    return (
+        <SectionProvider>
+            <UserProvider>
+                {children}
+            </UserProvider>
+        </SectionProvider>
+    )
+}
