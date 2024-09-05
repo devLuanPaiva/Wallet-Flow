e346bdb72f770727d204c3861beeb28c81234911
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-05 15:16:53 -0300

    feat: implements LoadingComponent

--- apps/frontend/src/app/(pages)/(internal)/layout.tsx
+++ apps/frontend/src/app/(pages)/(internal)/layout.tsx
@@ -2,6 +2,7 @@ import Page from "@/components/shared/Page";
 import UserForce from "@/components/shared/UserForce";
 import { AccountProvider } from "@/data/contexts/AccountContext";
 import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
 
 export default function Layout(props: any) {
     return (
@@ -9,6 +10,7 @@ export default function Layout(props: any) {
             <AccountProvider>
                 <Header />
                 <Page>{props.children}</Page>
+                <Footer/>
             </AccountProvider>
         </UserForce>
     )
