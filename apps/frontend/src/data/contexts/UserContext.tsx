8c478c05da87b20d8214430b2f87de069fef319a
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-10 09:36:45 -0300

    fix: show personalized error messages of user

--- apps/frontend/src/data/contexts/UserContext.tsx
+++ apps/frontend/src/data/contexts/UserContext.tsx
@@ -9,39 +9,46 @@ import { useRouter } from "next/navigation";
 const UserContext = createContext<UserContextProps>({} as any)
 
 export function UserProvider({ children }: any) {
-    const { httpPOST } = useAPI()
-    const { clearSection, createSection, loading, user } = useSection()
-    const router = useRouter();
-  
-    const register = useCallback(async (user: UserI) => {
+  const { httpPOST } = useAPI()
+  const { clearSection, createSection, loading, user } = useSection()
+  const router = useRouter();
+
+  const register = useCallback(async (user: UserI) => {
+    try {
       await httpPOST('user/register', user);
-    }, [httpPOST]);
-  
-    const login = useCallback(async (user: Partial<UserI>) => {
+    } catch (error: any) {
+      throw error.message
+    }
+  }, [httpPOST]);
+
+  const login = useCallback(async (user: Partial<UserI>) => {
+    try {
       const token = await httpPOST('user/login', user);
       createSection(token);
-    }, [createSection, httpPOST]);
-  
-    const logout = useCallback(() => {
-      clearSection();
-      router.push("/");
-    }, [router, clearSection]);
-  
-    const contextValue = useMemo(() => {
-      return {
-        loading,
-        user,
-        login,
-        register,
-        logout,
-      };
-    }, [loading, user, login, register, logout]);
-    return (
-      <UserContext.Provider value={contextValue}>
-        {children}
-      </UserContext.Provider>
-    );
-  }
-  
-  export default UserContext;
-  
+    } catch (error: any) {
+      throw error.message
+    }
+  }, [createSection, httpPOST]);
+
+  const logout = useCallback(() => {
+    clearSection();
+    router.push("/");
+  }, [router, clearSection]);
+
+  const contextValue = useMemo(() => {
+    return {
+      loading,
+      user,
+      login,
+      register,
+      logout,
+    };
+  }, [loading, user, login, register, logout]);
+  return (
+    <UserContext.Provider value={contextValue}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export default UserContext;
