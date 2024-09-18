d67e18caa4000e7dc629b863ff0abf843981e95f
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-18 10:26:50 -0300

    chore: env initial configurations

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,6 +1,7 @@
-module.exports = function (api) {
+export default function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: ["module:react-native-dotenv"],
   };
 };
