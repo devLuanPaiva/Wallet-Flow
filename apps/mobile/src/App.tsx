d3e74221fad9ac88f8d4ab8f094a3579036e93cb
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-19 10:38:17 -0300

    refactor: remove header

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -2,11 +2,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
 import Landing from './screens/Landing'
 const Stack = createNativeStackNavigator()
-export default function App(){
+export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName='Home'>
-                <Stack.Screen name='Landing' component={Landing} />
+                <Stack.Screen name='Landing' component={Landing}options={{ headerShown: false }} />
             </Stack.Navigator>
         </NavigationContainer>
     )
