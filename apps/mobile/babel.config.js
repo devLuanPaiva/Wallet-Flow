2da1808c3d716408cec78158c506fd17ea10cfdf
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-19 10:21:04 -0300

    fix: configure Babel correctly to support ESModules

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,7 +1,11 @@
-export default function (api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["module:react-native-dotenv"],
+    plugins: [
+      'module:react-native-dotenv',
+      'react-native-reanimated/plugin',
+    ]
+    
   };
 };
