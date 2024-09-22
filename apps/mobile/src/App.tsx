37b77569231238caa17d5e5519e93f69b6e6483b
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-22 09:05:10 -0300

    refactor: wrap application with UserProvider and SectionProvider

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -2,21 +2,28 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
 import Landing from './screens/Landing'
 import Access from './screens/Access'
+import { SectionProvider } from './data/contexts/SectionContext'
+import { UserProvider } from './data/contexts/UserContext'
 const Stack = createNativeStackNavigator()
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName='Home'>
-                <Stack.Screen
-                    name='Landing' component={Landing}
-                    options={{ headerShown: false }}
-                />
-                <Stack.Screen
-                    name='Access'
-                    component={Access}
-                    options={{ headerShown: false }}
-                />
-            </Stack.Navigator>
+            <SectionProvider>
+                <UserProvider>
+                    <Stack.Navigator initialRouteName='Home'>
+                        <Stack.Screen
+                            name='Landing' component={Landing}
+                            options={{ headerShown: false }}
+                        />
+                        <Stack.Screen
+                            name='Access'
+                            component={Access}
+                            options={{ headerShown: false }}
+                        />
+                    </Stack.Navigator>
+
+                </UserProvider>
+            </SectionProvider>
         </NavigationContainer>
     )
 }
