b5b5870578d6fd77f1e86056f2030352b62d2262
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-24 14:33:56 -0300

    feat: testing user register in the access screen

--- apps/mobile/__tests__/Access-test.tsx
+++ apps/mobile/__tests__/Access-test.tsx
@@ -1,15 +1,31 @@
-import { render, waitFor, fireEvent } from '@testing-library/react-native'
+import { render, waitFor, fireEvent } from '@testing-library/react-native';
 import Access from "../src/screens/Access";
-import fetchMock from 'jest-fetch-mock'
 import { SectionProvider } from '../src/data/contexts/SectionContext';
 import { UserProvider } from '../src/data/contexts/UserContext';
 import { NavigationContainer } from '@react-navigation/native';
+import Toast from 'react-native-toast-message';
 
-describe('UserContext and SectionContext Integration', () => {
+// Mock do Toast
+jest.mock('react-native-toast-message', () => ({
+    show: jest.fn(),
+}));
+
+// Mock do hook useAPI
+jest.mock('../src/data/hooks/useAPI', () => ({
+    __esModule: true,
+    default: jest.fn(() => ({
+        httpPOST: jest.fn().mockResolvedValue({}), // Mock de httpPOST que resolve sem erros
+    })),
+}));
+
+describe('Access Screen', () => {
+    const mockShowToast = Toast.show;
+
     beforeEach(() => {
-        fetchMock.resetMocks()
-    })
-    test('should render Access screen and handle login', () => {
+        jest.clearAllMocks();
+    });
+
+    test('should render Access screen', () => {
         const { getByText } = render(
             <NavigationContainer>
                 <UserProvider>
@@ -20,4 +36,32 @@ describe('UserContext and SectionContext Integration', () => {
 
         getByText('Gerencie suas finanças de maneira inteligente e segura. Cadastre-se para começar!');
     });
-})
+
+    test('should register a new user and show success toast', async () => {
+        const { getByText, getByPlaceholderText } = render(
+            <NavigationContainer>
+                <UserProvider>
+                    <SectionProvider>
+                        <Access />
+                    </SectionProvider>
+                </UserProvider>
+            </NavigationContainer>
+        );
+
+        fireEvent.press(getByText('Ainda não tem conta? Cadastre-se!'));
+        fireEvent.changeText(getByPlaceholderText('Digite seu nome'), 'Joana Doe');
+        fireEvent.changeText(getByPlaceholderText('Digite seu e-mail'), 'joanadoe123@gmail.com');
+        fireEvent.changeText(getByPlaceholderText('Digite sua senha'), '#Senha123');
+
+        fireEvent.press(getByText('Cadastrar'));
+
+        // Verifica se o toast de sucesso foi chamado corretamente
+        await waitFor(() => {
+            expect(mockShowToast).toHaveBeenCalledWith({
+                type: 'success',
+                text1: 'Registro bem-sucedido!',
+                text2: 'Você se registrou com sucesso.',
+            });
+        });
+    });
+});
