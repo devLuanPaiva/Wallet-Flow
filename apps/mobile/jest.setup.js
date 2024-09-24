cc1fb6266b2062b3a0b069cf5e5486deb0078fb0
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-24 14:35:24 -0300

    feat: add mock for asyncStorage

--- apps/mobile/jest.setup.js
+++ apps/mobile/jest.setup.js
@@ -1,4 +1,5 @@
 import 'react-native-gesture-handler/jestSetup';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Mock Native Modules that cause issues
 jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'));
@@ -20,3 +21,13 @@ jest.mock('react-native-safe-area-context', () => {
 });
 
 jest.mock('expo-linear-gradient', () => 'LinearGradient');
+
+
+jest.mock('@react-native-async-storage/async-storage', () => {
+  return {
+    getItem: jest.fn(() => Promise.resolve(null)),
+    setItem: jest.fn(() => Promise.resolve()),
+    removeItem: jest.fn(() => Promise.resolve()),
+    clear: jest.fn(() => Promise.resolve()),
+  };
+});
