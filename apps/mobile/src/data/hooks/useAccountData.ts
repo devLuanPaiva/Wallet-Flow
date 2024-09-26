9d04c1ecc03b7e2c6a257ab435cd7d4783e8c9d2
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-26 08:51:37 -0300

    feat: creat useAccountData hook

---
+++ apps/mobile/src/data/hooks/useAccountData.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { AccountI } from "@wallet/core";
+import useAccount from "./useAccount";
+
+export function useAccountData() {
+  const { fetchAccount } = useAccount();
+  const [account, setAccount] = useState<AccountI | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function loadAccount() {
+      try {
+        const fetchedAccount = await fetchAccount();
+        setAccount(fetchedAccount);
+      } catch (error) {
+        console.error("Erro ao carregar a conta: ", error);
+        setError("Erro ao carregar a conta");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadAccount();
+  }, [fetchAccount]);
+
+  return { account, loading, error };
+}
