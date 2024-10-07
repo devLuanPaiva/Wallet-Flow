2fee0324d59f141171ac52410fc5b91d662bb243
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-07 16:35:39 -0300

    feat: testing call createAccount with the correct data

--- apps/mobile/__tests__/CreateAccount-test.tsx
+++ apps/mobile/__tests__/CreateAccount-test.tsx
@@ -6,12 +6,13 @@ jest.mock('@/src/data/hooks/useAccount');
 
 describe("CreateAccount Component", () => {
     const mockCreateAccount = jest.fn();
+
     beforeEach(() => {
         (useAccount as jest.Mock).mockReturnValue({
-            transfer: mockCreateAccount,
+            createAccount: mockCreateAccount
         });
     });
-    
+
     it('should render correctly', () => {
         const { getByText } = render(<CreateAccount />);
         expect(getByText('Cadastre uma Conta')).toBeTruthy();
@@ -20,4 +21,19 @@ describe("CreateAccount Component", () => {
         expect(getByText('Criar Conta')).toBeTruthy();
     });
 
-})
+    it('should call createAccount with the correct data', async () => {
+        const { getByPlaceholderText, getByText } = render(<CreateAccount />);
+
+        fireEvent.changeText(getByPlaceholderText('Insira a chave de transferência'), '1234567890');
+        fireEvent.changeText(getByPlaceholderText('Insira o saldo inicial'), '1000');
+
+        fireEvent.press(getByText('Criar Conta'));
+
+        await waitFor(() => {
+            expect(mockCreateAccount).toHaveBeenCalledWith({
+                transferKey: BigInt('1234567890'),
+                bankBalance: 1000,
+            });
+        });
+    });
+});
