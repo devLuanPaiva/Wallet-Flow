2314405ef6f9ac4a21e8ec88c8acfb6fbdab959a
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-07 15:48:31 -0300

    feat: testing show success message on successful transfer

--- apps/mobile/__tests__/Transfer-test.tsx
+++ apps/mobile/__tests__/Transfer-test.tsx
@@ -55,4 +55,29 @@ describe('Transfer Component', () => {
             expect(mockTransfer).toHaveBeenCalledWith(250, 1, BigInt('0987654321'));
         });
     });
+
+    it('should show success message on successful transfer', async () => {
+        const accountProps: AccountProps['account'] = {
+            id: 1,
+            transferKey: '1234567890',
+            bankBalance: 1000,
+            user: {
+                id: 1,
+                email: 'test@email.com',
+                name: 'Teste'
+            }
+        };
+
+        const { getByPlaceholderText, getByText} = render(<Transfer account={accountProps} />);
+
+        fireEvent.changeText(getByPlaceholderText('Insira a chave de transferência'), '0987654321');
+        fireEvent.changeText(getByPlaceholderText('Insira o valor'), '250');
+
+        fireEvent.press(getByText('Transferir'));
+
+        await waitFor(() => {
+            expect(mockTransfer).toHaveBeenCalledWith(250, 1, BigInt('0987654321'));
+            expect(mockTransfer).toHaveBeenCalledTimes(2);
+        })
+    })
 });
