7f861d52432b6ec5cf07ccf8dc7e7362fa068a3b
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-07 15:35:01 -0300

    feat: testing call transfer function with correct parameters

--- apps/mobile/__tests__/Transfer-test.tsx
+++ apps/mobile/__tests__/Transfer-test.tsx
@@ -1,7 +1,6 @@
 import Transfer from "@/src/components/account/Transfer";
 import useAccount from "@/src/data/hooks/useAccount";
 import { render, fireEvent, waitFor } from "@testing-library/react-native";
-
 import { AccountProps } from "@/src/data/interfaces";
 
 jest.mock('@/src/data/hooks/useAccount');
@@ -11,7 +10,7 @@ describe('Transfer Component', () => {
 
     beforeEach(() => {
         (useAccount as jest.Mock).mockReturnValue({
-            deposit: mockTransfer,
+            transfer: mockTransfer,
         });
     });
 
@@ -25,11 +24,35 @@ describe('Transfer Component', () => {
                 email: 'test@email.com',
                 name: 'Teste'
             }
-        }
+        };
         const { getByText } = render(<Transfer account={accountProps} />);
         expect(getByText('Transferir Dinheiro')).toBeTruthy();
         expect(getByText('Chave de Transferência')).toBeTruthy();
         expect(getByText('Valor da Transferência')).toBeTruthy();
         expect(getByText('Transferir')).toBeTruthy();
     });
-})
+
+    it('should call transfer function with correct parameters', async () => {
+        const accountProps: AccountProps['account'] = {
+            id: 1,
+            transferKey: '1234567890',
+            bankBalance: 1000,
+            user: {
+                id: 1,
+                email: 'test@email.com',
+                name: 'Teste'
+            }
+        };
+
+        const { getByPlaceholderText, getByText } = render(<Transfer account={accountProps} />);
+
+        fireEvent.changeText(getByPlaceholderText('Insira a chave de transferência'), '0987654321');
+        fireEvent.changeText(getByPlaceholderText('Insira o valor'), '250');
+
+        fireEvent.press(getByText('Transferir'));
+
+        await waitFor(() => {
+            expect(mockTransfer).toHaveBeenCalledWith(250, 1, BigInt('0987654321'));
+        });
+    });
+});
