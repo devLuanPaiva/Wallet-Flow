cb16a5ac735b277fb8da92d2c235941444f4f913
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-17 14:12:04 -0300

    refactor: change bg color of cards

--- apps/frontend/src/components/ui/card-hover-effect.tsx
+++ apps/frontend/src/components/ui/card-hover-effect.tsx
@@ -19,7 +19,7 @@ export const HoverEffect = ({
     return (
         <div
             className={cn(
-                "grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3  py-10",
+                "grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 py-10", 
                 className
             )}
         >
@@ -27,14 +27,14 @@ export const HoverEffect = ({
                 <Link
                     href={item?.link}
                     key={item?.link}
-                    className="relative group  block p-2 h-full w-full"
+                    className="relative group block p-4 h-full w-full"
                     onMouseEnter={() => setHoveredIndex(idx)}
                     onMouseLeave={() => setHoveredIndex(null)}
                 >
                     <AnimatePresence>
                         {hoveredIndex === idx && (
                             <motion.span
-                                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block  rounded-3xl"
+                                className="absolute inset-0 h-full w-full bg-purple-300 dark:bg-purple-800/[0.8] block rounded-3xl"
                                 layoutId="hoverBackground"
                                 initial={{ opacity: 0 }}
                                 animate={{
@@ -68,7 +68,7 @@ export const Card = ({
     return (
         <div
             className={cn(
-                "rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
+                "rounded-2xl max-h-72 max-w-xs w-full p-4 overflow-hidden bg-purple-950 border border-transparent dark:border-white/[0.2] group-hover:border-purple-950 relative z-20", 
                 className
             )}
         >
@@ -78,6 +78,7 @@ export const Card = ({
         </div>
     );
 };
+
 export const CardTitle = ({
     className,
     children,
@@ -86,11 +87,12 @@ export const CardTitle = ({
     children: React.ReactNode;
 }) => {
     return (
-        <h4 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>
+        <h4 className={cn("text-zinc-100 font-bold tracking-wide mt-2", className)}>
             {children}
         </h4>
     );
 };
+
 export const CardDescription = ({
     className,
     children,
@@ -101,7 +103,7 @@ export const CardDescription = ({
     return (
         <p
             className={cn(
-                "mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm",
+                "mt-4 text-zinc-400 tracking-wide leading-relaxed text-sm text-justify", 
                 className
             )}
         >
