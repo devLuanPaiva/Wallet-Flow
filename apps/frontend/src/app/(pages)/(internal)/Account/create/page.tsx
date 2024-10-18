35f9117470e3f20bc632a13e06992d5e4425b742
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-18 16:33:04 -0300

    feat: add title in the create account page

--- apps/frontend/src/app/(pages)/(internal)/Account/create/page.tsx
+++ apps/frontend/src/app/(pages)/(internal)/Account/create/page.tsx
@@ -1,8 +1,10 @@
 import AccountForm from "@/components/account/AccountForm";
+import TitleAnimated from "@/components/shared/TitleAnimated";
 
 export default function CreateAccount(){
     return(
-        <div className="flex flex-col items-center  p-4">         
+        <div className="flex flex-col items-center p-4">
+            <TitleAnimated titleTop="Adicione sua" titleBottom="conta bancária" slogan="Conecte sua conta e aproveite todos os benefícios da sua carteira digital!"/>         
             <AccountForm/>
         </div>
     )
