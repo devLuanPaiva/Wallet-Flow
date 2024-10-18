bf973f84f900a1cc8ec3ba3e28fe8292b718b3d3
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-18 16:28:58 -0300

    refactor: remove Header main

--- apps/frontend/src/app/(pages)/(internal)/layout.tsx
+++ apps/frontend/src/app/(pages)/(internal)/layout.tsx
@@ -1,14 +1,12 @@
 import Page from "@/components/shared/Page";
 import UserForce from "@/components/shared/UserForce";
 import { AccountProvider } from "@/data/contexts/AccountContext";
-import Header from "@/components/shared/Header";
 import Footer from "@/components/shared/Footer";
 
 export default function Layout(props: any) {
     return (
         <UserForce>
             <AccountProvider>
-                <Header />
                 <Page>{props.children}</Page>
                 <Footer/>
             </AccountProvider>
