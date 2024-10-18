69721a56bd5f84fe350c96fc415d1833706b02f7
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-18 16:31:08 -0300

    refactor: improve the responsiveness of the main page

--- apps/frontend/src/components/shared/Page.tsx
+++ apps/frontend/src/components/shared/Page.tsx
@@ -1,10 +1,11 @@
+
 interface PageProps {
     children: React.ReactNode;
 }
 export default function Page(props: Readonly<PageProps>) {
     return (
-        <div className="flex flex-col ">
-            <main className="min-h-screen bg-purple-200">{props.children}</main>
+        <div className="flex flex-col bg-gradient-to-b from-purple-50 via-purple-100 to-purple-300">
+            <main className="min-h-screen w-full box-border md:w-4/5 m-auto flex-1 py-3 px-4 ">{props.children}</main>
         </div>
     )
 }
