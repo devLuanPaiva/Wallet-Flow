315a0191a3aed9adf742c4356b474eedd438522a
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-26 08:21:14 -0300

    feat: testing show error when the key length is different from ten

--- apps/frontend/__tests__/accountForm.test.tsx
+++ apps/frontend/__tests__/accountForm.test.tsx
@@ -56,5 +56,26 @@ describe('AccountForm Component', () => {
         fireEvent.click(confirmButton)
 
         await waitFor(() => expect(screen.getByText('Sucesso')).toBeInTheDocument())
+        expect(mockCreateAccount).toHaveBeenCalledWith({ "bankBalance": 1000, "transferKey": "1234567890" })
+    })
+    it('should show error when the key length is different from ten', async () => {
+        mockCreateAccount.mockRejectedValueOnce(new Error("A chave de transferência deve ter exatamente 10 dígitos."))
+        render(
+            <AccountProvider>
+                <AccountForm />
+            </AccountProvider>
+        )
+        const inputTransferKey = screen.getByPlaceholderText('Chave de Transferência')
+        fireEvent.change(inputTransferKey, { target: { value: '1234' } })
+        const inputInitialBalance = screen.getByPlaceholderText('Saldo Inicial')
+        fireEvent.change(inputInitialBalance, { target: { value: accountSubmit.bankBalance } })
+        fireEvent.click(screen.getByText('Cadastrar'))
+
+        const confirmButton = await screen.findByText('Confirmar')
+        fireEvent.click(confirmButton)
+
+        await waitFor(() => expect(screen.getByText('Erro')).toBeInTheDocument())
+        await waitFor(() => expect(screen.getByText(/a chave de transferência deve ter exatamente 10 dígitos/i)).toBeInTheDocument())
+
     })
 })
